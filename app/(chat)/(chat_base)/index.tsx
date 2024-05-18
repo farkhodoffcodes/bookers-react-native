490729e57d221d84fa80c07f6a8f6a75c46126a4
farkhodoffcodes <farkhodoffcodes@gmail.com>
2024-05-18 23:20:28 +0500

    file structure

---
+++ app/(chat)/(chat_base)/index.tsx
@@ -0,0 +1,5 @@
+export default function ChatBase() {
+    return (
+        <ThemedText type="title">Master</ThemedText>
+    )
+}
---
+++ app/(chat)/(chat_detail)/index.tsx
@@ -0,0 +1,5 @@
+export default function ChatDetails() {
+    return (
+        <ThemedText type="title">Master</ThemedText>
+    )
+}
