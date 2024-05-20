24719bdfc9ef36383291af0093a6aefdf3d0636a
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-05-20 13:47:21 +0500

    salom

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout() {
 	return (
 		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
 			<Stack initialRouteName="(tabs)">
-				<Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
+				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
 				<Stack.Screen name="(_landing)/" options={{ headerShown: false }} />
 				<Stack.Screen name="+not-found" />
 			</Stack>
