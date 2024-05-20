00da2e1b103a927e83e38a3aaf2549f0c54237a5
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-05-20 12:20:00 +0500

    blet z

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout() {
 	return (
 		<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
 			<Stack initialRouteName="(tabs)">
-				<Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-				<Stack.Screen name="(_landing)/landing" options={{ headerShown: false }} />
+				<Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
+				<Stack.Screen name="(_landing)/" options={{ headerShown: false }} />
 				<Stack.Screen name="+not-found" />
 			</Stack>
 		</ThemeProvider>
