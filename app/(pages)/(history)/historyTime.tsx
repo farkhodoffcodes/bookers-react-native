cc2712fa95b84b97807898e2ff60426eb539f421
omadbekolimov <omadbekolimov96@gmail.com>
2024-05-20 13:07:52 +0500

    gooo

---
+++ app/(pages)/(history)/historyTime.tsx
@@ -0,0 +1,16 @@
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+import tw from 'tailwind-react-native-classnames'
+
+const HistoryTime = () => {
+  return (
+    <ScrollView>
+     <View style={tw`flex-1 justify-center items-center bg-gray-900 p-4`}>
+      
+     </View>
+    </ScrollView>
+    
+  )
+}
+
+export default HistoryTime;
