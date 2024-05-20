4de13885bf4b152537e687a28bb040b4fac0f746
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-05-20 11:50:35 +0500

    zayball

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,63 +6,64 @@ import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
 export default function TabLayout() {
+  
   const colorScheme = useColorScheme();
 
   return (
     <Tabs
-    initialRouteName="index" // Dastlab ochiladigan ekran
-    screenOptions={{
-      tabBarActiveTintColor: '#9C0A35',
-      tabBarInactiveTintColor: 'gray',
-      tabBarStyle: { backgroundColor: '#1C1C1E' },
-      headerShown: false,
-    }}
-  >
-    <Tabs.Screen
-      name="index"
-      options={{
-        title: 'Home',
-        tabBarIcon: ({ color, focused }) => (
-          <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-        ),
+      initialRouteName="index" // Dastlab ochiladigan ekran
+      screenOptions={{
+        tabBarActiveTintColor: '#9C0A35',
+        tabBarInactiveTintColor: 'gray',
+        tabBarStyle: { backgroundColor: '#1C1C1E' },
+        headerShown: false,
       }}
-    />
-    <Tabs.Screen
-      name="explore"
-      options={{
-        title: 'Explore',
-        tabBarIcon: ({ color, focused }) => (
-          <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-        ),
-      }}
-    />
-    <Tabs.Screen
-      name="services"
-      options={{
-        title: 'Services',
-        tabBarIcon: ({ color, focused }) => (
-          <TabBarIcon name={focused ? 'construct' : 'construct-outline'} color={color} />
-        ),
-      }}
-    />
-    <Tabs.Screen
-      name="chat"
-      options={{
-        title: 'Chat',
-        tabBarIcon: ({ color, focused }) => (
-          <TabBarIcon name={focused ? 'chatbubble' : 'chatbubble-outline'} color={color} />
-        ),
-      }}
-    />
-    <Tabs.Screen
-      name="profile"
-      options={{
-        title: 'Profile',
-        tabBarIcon: ({ color, focused }) => (
-          <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
-        ),
-      }}
-    />
-  </Tabs>
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="services"
+        options={{
+          title: 'Services',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'construct' : 'construct-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="chat"
+        options={{
+          title: 'Chat',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'chatbubble' : 'chatbubble-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'person' : 'person-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
