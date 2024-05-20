6c681fd958ff090314edc8caa71c953fd4255623
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-05-20 01:39:15 -0700

    pizdes nax

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,11 @@
-import { Image, StyleSheet, Platform, View } from 'react-native';
+import { Image, StyleSheet, Platform, View } from "react-native";
 
-import Landing from '../(_landing)/landing';
-
-export default function HomeScreen() {
-  return (
-  <View>
-    <Landing/>
-  </View>
-  );
+import Landing from "../(_landing)/landing";
+const HomeScreen=()=> {
+	return (
+		<View>
+			<Landing />
+		</View>
+	);
 }
-
+export default Landing
