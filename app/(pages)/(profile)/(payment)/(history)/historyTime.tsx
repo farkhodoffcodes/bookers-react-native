0449d1e1fc7e25871ab10dca66add323e7702529
omadbekolimov <omadbekolimov96@gmail.com>
2024-05-20 22:55:31 +0500

    history

--- app/(pages)/(profile)/(payment)/(history)/historyTime.tsx
+++ app/(pages)/(profile)/(payment)/(history)/historyTime.tsx
@@ -9,15 +9,19 @@ import CardsDatail from '@/components/cards/cardsDatil'
 const HistoryTime = () => {
   return (
     <ScrollView style={tw`h-1000 bg-dark`}> 
-    <View style={tw`flex-1 justify-center items-center bg-gray-900 p-4 h-screen w-full`}>
+    <View style={tw`flex-1  bg-gray-900 p-4 h-screen w-full`}>
        <Navbar back={true} name="История сеансов" icons={false} centerName={true} />
-
+       <View style={tw`bg-gray-700 px-3 py-4 rounded-xl`}>
+          <Text style={tw`text-red-700 fw-bold flex flex-row flex-col font-sans`}>
+             Предстоящие
+          </Text>
+        </View>
         <View style={tw`w-full`}>
           <RecordsCard name='Пн, 10 февраля 12:30 - 13:30 ' label='Наращивание ресниц' pageRoute='nima'/>
         </View>
+        
         <View style={tw`w-full mt-3`}>
           <RecordsCard name='Вт, 11 февраля 14:30 - 15:50  ' label='Стрижка, укладка, покраска волос' pageRoute='nima'/>
-          <CardsDatail name='nima' alias='nima' rating={3} location='nima' imageUrl='' orders={2} orderZakaz='20' orderKlent='20' clients={20} master='wer'/>
         </View>
        
      </View>
