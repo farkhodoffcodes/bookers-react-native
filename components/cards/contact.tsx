4f388b385725a33f74c287db01b9a4084091f70b
omadbekolimov <omadbekolimov96@gmail.com>
2024-05-21 13:04:22 +0500

    gooo

--- components/cards/contact.tsx
+++ components/cards/contact.tsx
@@ -4,7 +4,7 @@ import { AntDesign, FontAwesome, Feather } from '@expo/vector-icons'; // Ikonlar
 import tw from 'tailwind-react-native-classnames';
 
 
-interface ContactInfoProps {
+type ContactInfoProps ={
     phone: string;
     instagram: string;
     telegram: string;
@@ -13,15 +13,15 @@ interface ContactInfoProps {
 const Contact: React.FC<ContactInfoProps> = ({ phone, instagram, telegram }) => {
   return (
     <View style={tw`bg-gray-200 rounded-2xl p-6`}>
-    <TouchableOpacity onPress={() => Linking.openURL(`tel:${phone}`)} style={tw`flex-row items-center mb-2 font-bold`}>
+    <TouchableOpacity onPress={() => Linking.openURL(`tel:${phone}`)} style={tw`flex-row items-center mb-2 font-bold text-dark`}>
       <AntDesign name="phone" size={26} color="#9C0A35" />
       <Text style={tw`ml-2 text-blue-700`}>{phone}</Text>
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => Linking.openURL(`https://www.instagram.com/${instagram}`)} style={tw`flex-row items-center mb-2 font-bold`}>
+    <TouchableOpacity onPress={() => Linking.openURL(`https://www.instagram.com/${instagram}`)} style={tw`flex-row items-center mb-2 font-bold text-dark`}>
       <FontAwesome name="instagram" size={26} color="#9C0A35" />
       <Text style={tw`ml-2 text-gray-700`}>@{instagram}</Text>
     </TouchableOpacity>
-    <TouchableOpacity onPress={() => Linking.openURL(`https://t.me/${telegram}`)} style={tw`flex-row items-center font-bold`}>
+    <TouchableOpacity onPress={() => Linking.openURL(`https://t.me/${telegram}`)} style={tw`flex-row items-center font-bold text-dark`}>
       <Feather name="send" size={26} color="#9C0A35" />
       <Text style={tw`ml-2 text-gray-700`}>@{telegram}</Text>
     </TouchableOpacity>
