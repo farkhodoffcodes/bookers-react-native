51cc7850cb70ddbbdad77cae045c28aa652b856a
omadbekolimov <omadbekolimov96@gmail.com>
2024-05-21 11:35:59 +0500

    HistoryUser Start

---
+++ app/(pages)/(profile)/(payment)/(history)/historyUser.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const HistoryUser = () => {
+  return (
+    <View>
+      <Text>HistoryUser</Text>
+    </View>
+  )
+}
+
+export default HistoryUser
