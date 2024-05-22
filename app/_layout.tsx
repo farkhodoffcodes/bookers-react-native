c86e74ae1a70d8639038666bb025722695ce7937
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-05-22 16:52:45 +0300

    aaaa

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,7 +33,6 @@ export default function RootLayout() {
 				<Stack.Screen name="(auth)/firstWelcome" options={{ headerShown: false }} />
 				<Stack.Screen name="(auth)/secondWelcome" options={{ headerShown: false }} />
 				<Stack.Screen name="(pages)/(notification)/index" options={{ headerShown: false }} />
-<<<<<<< HEAD
 				<Stack.Screen
 					name="(pages)/(profile)/(payment)/(card_page)/card_page"
 					options={{ headerShown: false }}
@@ -42,11 +41,9 @@ export default function RootLayout() {
 					name="(pages)/(profile)/(payment)/(create_card)/create-card"
 					options={{ headerShown: false }}
 				/>
-=======
 				<Stack.Screen name="(pages)/(profile)/(personal_data)/personal_data" options={{ headerShown: false }} />
 				<Stack.Screen name="(pages)/(profile)/(payment)/(card_page)/card_page" options={{ headerShown: false }} />
 				<Stack.Screen name="(pages)/(profile)/(payment)/(create_card)/create_card" options={{ headerShown: false }} />
->>>>>>> b982d8a8c4eeed74952ca75ddef5e87d97782f4b
 				<Stack.Screen name="(_landing)/" options={{ headerShown: false }} />
 				<Stack.Screen name="(_landing)/map" options={{ headerShown: false }} />
 				<Stack.Screen name="(auth)/auth" options={{ headerShown: false }} />
