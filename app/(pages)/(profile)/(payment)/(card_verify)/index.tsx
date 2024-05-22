c1b19b0ec642986f91f7b0f7db1163fa9c4f893a
abdulazizsaidovcode <abdulazizbey5@gmail.com>
2024-05-22 19:43:32 +0500

    otp input boshlandi

--- app/(pages)/(profile)/(payment)/(card_verify)/index.tsx
+++ app/(pages)/(profile)/(payment)/(card_verify)/index.tsx
@@ -1,7 +1,11 @@
 import { ThemedText } from "@/components/ThemedText";
+import { View } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 export default function CartVerify() {
     return (
-        <ThemedText type="title">CartVerify</ThemedText>
+        <View style={tw`flex-1 bg-black`}>
+
+        </View>
     )
 }
