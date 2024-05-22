29d2c4620c192e2bceadd616d0c136cdde7ecbed
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-05-22 14:04:39 +0500

    notification page full

--- components/cards/notification_card.tsx
+++ components/cards/notification_card.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableWithoutFeedback } from 'react-native'
 import React from 'react'
 import { Feather } from '@expo/vector-icons';
 
@@ -7,31 +7,40 @@ interface INotificationProps {
   title: string;
   description: string;
   date: string;
+  setData?: (data: any) => void;
+  onPress?: () => void;
 }
 
 const NotificationCard: React.FC<INotificationProps> = (props) => {
-  const { iconJsxElement, title, description, date } = props;
+  const { iconJsxElement, title, description, date, setData, onPress } = props;
   return (
-    <View
-      style={styles.card}
-      className={`bg-[#B9B9C9] p-5 rounded-3xl w-full flex-row items-start justify-start mb-4`}
+    <TouchableWithoutFeedback
+      onPress={() => {
+        setData && setData({ title, description, date })
+        onPress && onPress()
+      }}
     >
-      <View className='w-[15%]'>
-        <View className='w-10 h-10 justify-center items-center rounded-full bg-white'>
-          {iconJsxElement}
+      <View
+        style={styles.card}
+        className={`bg-[#B9B9C9] p-5 rounded-3xl w-full flex-row items-start justify-start mb-4`}
+      >
+        <View className='w-[15%]'>
+          <View className='w-10 h-10 justify-center items-center rounded-full bg-white'>
+            {iconJsxElement}
+          </View>
+        </View>
+        <View className='w-[80%]'>
+          <Text className={`text-[20px] font-[900] text-black mt-1 mb-4`}>{title}</Text>
+          <Text className={`text-[17px] text-black font-[600] opacity-50 mb-3 pr-5`}>
+            {description.length > 50 ? description.slice(0, 50) + '...' : description}
+          </Text>
+          <View className={`flex-row justify-between items-center`}>
+            <Text className={`text-[16px] text-black opacity-40`}>{date}</Text>
+            <Feather name="chevron-right" size={25} color="gray" />
+          </View>
         </View>
       </View>
-      <View className='w-[80%]'>
-        <Text className={`text-[20px] font-[900] text-black mt-1 mb-4`}>{title}</Text>
-        <Text className={`text-[17px] text-black font-[600] opacity-50 mb-3 pr-5`}>
-          {description.length > 50 ? description.slice(0, 50) + '...' : description}
-        </Text>
-        <View className={`flex-row justify-between items-center`}>
-          <Text className={`text-[16px] text-black opacity-40`}>{date}</Text>
-          <Feather name="chevron-right" size={25} color="gray" />
-        </View>
-      </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
