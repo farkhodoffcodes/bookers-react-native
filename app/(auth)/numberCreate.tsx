eed67e8edee021a7dc6ec7c061bfddba7019ddf8
SardorbekCoder07 <sardorbekcoderjon@gmail.com>
2024-05-22 17:15:25 +0300

    jigarla npm i qilvolla

--- app/(auth)/numberCreate.tsx
+++ app/(auth)/numberCreate.tsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import ModalButton from '@/components/(buttons)/modal-btn';
+import { router } from 'expo-router';
+import Navbar from '../(navigation)/navbar';
 
 const NumberCreate = () => {
   return (
-    <View>
-      <Text>NumberCreate</Text>
-    </View>
+    <>
+      <View className='bg-[#1E1E1E] '>
+        <View className='mt-5'>
+          <Navbar name="" back={true} icons={false} centerName={false} />
+        </View>
+      </View>
+      <SafeAreaView style={styles.container}>
+        <View className='items-center'>
+          <Text className='text-white text-lg mb-3'>Ваш номер телефона</Text>
+          <Text className='text-[#4F4F4F] text-center'>Мы отправили вам SMS с кодом подтверждения.</Text>
+        </View>
+      </SafeAreaView>
+    </>
+
   )
 }
 
-export default NumberCreate
+export default NumberCreate;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1E1E1E',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+})
