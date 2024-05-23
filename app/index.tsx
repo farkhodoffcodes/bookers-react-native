c59e0d242f1d56b0c56ffd3334925e48ffe4526f
PABLO <temirovmuhammadyusuf70@gmail.com>
2024-05-23 14:56:47 +0500

    til qoshildi

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,18 @@ import { View, Text, StyleSheet, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import ModalButton from '@/components/(buttons)/modal-btn';
 import { router } from 'expo-router';
+import { useTranslation } from 'react-i18next';
 
 const Index = () => {
+    const { i18n } = useTranslation();
+
     return (
         <SafeAreaView style={styles.container}>
             <View style={styles.logoContainer}>
                 <Image resizeMode='contain' source={require('../assets/images/logo.png')} />
             </View>
             <View>
-                <Text style={{ fontSize: 20, color: 'white', marginTop: 15 , marginBottom:8 }}>Bookers Beauty</Text>
+                <Text style={{ fontSize: 20, color: 'white', marginTop: 15, marginBottom: 8 }}>Bookers Beauty</Text>
             </View>
             <View>
                 <Text style={{ fontSize: 21, color: 'white', marginTop: 40 }}>Добро пожаловать!</Text>
@@ -19,14 +22,23 @@ const Index = () => {
                 <Text style={{ fontSize: 22, color: 'white', marginTop: 40 }}>Выберите язык</Text>
             </View>
             <View style={{ width: '90%' }}>
-                <View style={{marginTop: 10}}>
-                    <ModalButton onPress={() => router.push('/(pages)/(profile)/(personal_data)/personal_data')} title='Русский' textColor='white' backgroundColor='#9C0A35' />
+                <View style={{ marginTop: 10 }}>
+                    <ModalButton onPress={() => {
+                        i18n.changeLanguage('ru')
+                        router.push('/firstWelcome')
+                    }} title='Русский' textColor='white' backgroundColor='#9C0A35' />
                 </View>
-                <View style={{marginTop: 10}}>
-                    <ModalButton onPress={() => router.push('/auth')} title="O'zbek" textColor='white' backgroundColor='#9C0A35' />
+                <View style={{ marginTop: 10 }}>
+                    <ModalButton onPress={() => {
+                        i18n.changeLanguage('uz')
+                        router.push('/firstWelcome')
+                    }} title="O'zbek" textColor='white' backgroundColor='#9C0A35' />
                 </View>
-                <View style={{marginTop: 10}}>
-                    <ModalButton onPress={() => router.push('/(pages)/(profile)/(payment)/(card_page)/card_page/')} title='English' textColor='white' backgroundColor='#9C0A35' />
+                <View style={{ marginTop: 10 }}>
+                    <ModalButton onPress={() => {
+                        i18n.changeLanguage('en')
+                        router.push('/firstWelcome')
+                    }} title='English' textColor='white' backgroundColor='#9C0A35' />
                 </View>
             </View>
         </SafeAreaView>
