e19fad844d5b9b553494001c2528d7e03fef5852
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-05-23 18:20:43 +0500

    route edit

---
+++ app/(pages)/(profile)/(personal_data)/date.tsx
@@ -0,0 +1,27 @@
+import { View, ScrollView } from 'react-native'
+import React from 'react'
+import NotificationNav from '@/components/navigation/notification_nav'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useNavigation } from 'expo-router'
+
+const UserDate = () => {
+    const navigation = useNavigation();
+    return (
+        <View className="bg-[#1E1E1E] flex-1 px-3 min-h-screen">
+            <SafeAreaView>
+                <NotificationNav
+                    name="Date"
+                    deleteIcon={false}
+                    backIcon={() => navigation.goBack()}
+                />
+            </SafeAreaView>
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 20 }}
+            >
+            </ScrollView>
+        </View>
+    )
+}
+
+export default UserDate
