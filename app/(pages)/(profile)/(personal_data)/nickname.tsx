e9bf75fb898defd32accc98acf54f10e64f3238c
alishersodiqov09 <alishersodiqov09@gmail.com>
2024-05-23 18:17:48 +0500

    profile xam oz qoldi

--- app/(pages)/(profile)/(personal_data)/nickname.tsx
+++ app/(pages)/(profile)/(personal_data)/nickname.tsx
@@ -4,9 +4,10 @@ import NotificationNav from '@/components/navigation/notification_nav'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useNavigation } from 'expo-router'
 import CustomInput from '@/components/inputs/nickname_input'
+import ModalButton from '@/components/(buttons)/modal-btn'
 
 const NickName = () => {
-    const navigation = useNavigation();    
+    const navigation = useNavigation();
     return (
         <View className="bg-[#1E1E1E] flex-1 px-3 min-h-screen">
             <SafeAreaView>
@@ -18,10 +19,26 @@ const NickName = () => {
             </SafeAreaView>
             <ScrollView
                 showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 20, alignItems: 'center' }}
+                contentContainerStyle={{ paddingBottom: 20 }}
             >
-                <CustomInput />
+                <Text className='text-2xl text-white font-bold mt-2 mb-4'>Придумайте свой</Text>
+                <CustomInput
+                    key={1}
+                    placeholder='Никнейм'
+                    valLength={5}
+                    i18nIsDynamicList
+                />
+                <Text className='text-base text-white mt-5 opacity-70'>Будем использовать вместо вашего имени</Text>
             </ScrollView>
+            <View className='absolute bottom-5 right-3 left-3'>
+                <ModalButton
+                    backgroundColor='#9C0A35'
+                    title='Готово'
+                    textColor='white'
+                    key={1}
+                    i18nIsDynamicList
+                />
+            </View>
         </View>
     )
 }
